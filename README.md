0ff1eb91fb21e81493d47559d9488d789fcc9b6d
Jerry Gamblin <jerry.gamblin@gmail.com>
2018-08-27 15:23:38 -0500

    First Push
    First Push

--- README.md
+++ README.md
@@ -1,29 +1,31 @@
-# Black Hat macOS Config
-A simple shell script to configure your Mac to some of the most commonly recommended settings for the world's most dangerous hacking conferences (Black Hat/Defcon) or every day life.
-
-**Warning:** Tool does not ask before making changes.
+# MacOS Security Baseline
+This is a simple shell script that configures some of the most commonly recommended MacOS security settings that are not enabled by default.
 
 ## What It Does:
-- Deletes Saved Wireless Networks.
-- ~~Disable Wireless Network Auto Join.~~ < This cant be done programmatically. `¯\_(ツ)_/¯`
-- Require Password Immediately After Sleep
-- Turns On Firewall.
-  - Enables Stealth Mode
-- Installs Needed System Updates.
-- Validates System Integrity Protection is enabled.
-- Enables Full Disk Encryption.
+-   Deletes Saved Wireless Networks.
+-   Requires Password Immediately After Sleep.
+-   Turns On Firewall.
+ -   Enables Stealth Mode.
+-   Disables Remote Login
+-   Installs Needed System Updates.
+ -   Enables Automatic Updates
+-   Validates System Integrity Protection is enabled.
+-   Enables Full Disk Encryption.
 
 Note: Script Will Prompt For Password If Not Root.
 
 ## Usage:
-- Review for your preferences and comment out options **You** don't want.
-- Set `homessid` and `workssid` variable to stop from deleting those!
+-   Review for your preferences and comment out options **You** don't want.
+-   Set `homessid` and `workssid` variable to stop from deleting these!
+-   **Warning:** This script does not ask before making changes.
 
-To Run:  
+**To Run:**
+
 ```
 chmod +x blackhat-macos-config.sh
 ./blackhat-macos-config.sh
 ```
 
+
 ## Important Notice
-I likely don't know what I am doing and this could be done faster, better and simpler some other way. These scripts could also break your MacBook and make you cry.
+I likely don't know what I am doing, and this could be done faster, better and simpler some other way. These scripts could also break your MacBook and make you cry.
--- security-baseline.sh
+++ security-baseline.sh
@@ -11,23 +11,23 @@ NC='\033[0m'
 #Check if running as root and if not elevate
 sudo -nv 2>>/dev/null
 if [ $? -ne 0 ]; then
-    printf "Black Hat macOS Config requires root access.\n"
-    printf "Please enter your password, or run 'sudo -v' first.\n"
+    printf "Black Hat macOS Config requires root access.\\n"
+    printf "Please enter your password, or run 'sudo -v' first.\\n"
     sudo -v
 
     #Validate we can sudo now
     if [ $? -ne 0 ]; then
-      printf "\n"
-      printf "Still not root. Exiting.\n"
+      printf "\\n"
+      printf "Still not root. Exiting.\\n"
       exit
     fi
-    printf "\n"
+    printf "\\n"
 fi
 
 #Delete Saved SSIDs For Security
 #Be Sure To Set Home And Work SSID for ease of use.
-printf "Deleting saved Wi-Fi networks.\n"
-IFS=$'\n'
+printf "Deleting saved Wi-Fi networks.\\n"
+IFS=$'\\n'
 for ssid in $(networksetup -listpreferredwirelessnetworks en0 | grep -v "Preferred networks on en0:" | grep -v $homessid | grep -v $workssid | sed "s/[\	]//g")
 do
     networksetup -removepreferredwirelessnetwork en0 "$ssid" > /dev/null 2>&1
@@ -35,47 +35,55 @@ done
 
 
 #Enable Password Login:
-printf "Require Password Immediately After Sleep or Screen Saver Begins.\n"
+printf "Require Password Immediately After Sleep or Screen Saver Begins.\\n"
 defaults write com.apple.screensaver askForPassword 1 > /dev/null 2>&1
 defaults write com.apple.screensaver askForPasswordDelay 0 > /dev/null 2>&1
 
-#Enable Firewall:
-printf "Enabling Firewall.\n"
+#Enables Secure SecureKeyboard Entry.
+#https://developer.apple.com/library/archive/technotes/tn2150/_index.html
+defaults write com.apple.Terminal SecureKeyboardEntry -bool true
 
-#This is a more "open" firewall config
+#Enable Firewall:
+printf "Enabling Firewall.\\n"
+
+#This is a more "open" firewall config.
 #https://discussions.apple.com/thread/3148672
 defaults write /Library/Preferences/com.apple.alf globalstate 1  > /dev/null 2>&1
 
 #This is a more "strict" firewall config
 #defaults write /Library/Preferences/com.apple.alf globalstate 2  > /dev/null 2>&1
-
-printf "Enabling Stealth Firewall Mode.\n"
+printf "Enabling Stealth Firewall Mode.\\n"
 defaults write /Library/Preferences/com.apple.alf stealthenabled 1 > /dev/null 2>&1
 
-
 #Install Updates.
-printf "Installing needed updates.\n"
+printf "Installing needed updates.\\n"
 softwareupdate -i -a > /dev/null 2>&1
 
+#Turn On Automatic Updates.
+printf "Enable Automatic Updates"
+softwareupdate --schedule on > /dev/null 2>&1
+
+#Disable Remote Login
+printf "Disable Remote Logins"
+systemsetup -f -setremotelogin off > /dev/null 2>&1
 
 #Check if System Integrity Protection is enabled
-printf "Verifying System Integrity Protection (SIP) is enabled.\n"
+printf "Verifying System Integrity Protection (SIP) is enabled.\\n"
 csrutil=$(csrutil status)
 if [[ $csrutil = *"disabled"* ]]; then
-  printf "${RED}WARNING: System Integrity Protection is disabled!${NC}\n"
-  printf "To enable, you must boot into recovery mode and enable:\n"
-  printf " - restart\n"
-  printf " - during bootup, hold Cmd+R\n"
-  printf " - click Utilities->Terminal\n"
-  printf " - run: csrutil enable\n"
-  printf " - run: reboot # to get back into standard macOS\n"
+  printf "${RED}WARNING: System Integrity Protection is disabled!${NC}\\n"
+  printf "To enable, you must boot into recovery mode and enable:\\n"
+  printf " - restart\\n"
+  printf " - during bootup, hold Cmd+R\\n"
+  printf " - click Utilities->Terminal\\n"
+  printf " - run: csrutil enable\\n"
+  printf " - run: reboot # to get back into standard macOS\\n"
 fi
 
 #Enabling Firevault:
-printf "Enabling FDE.\n"
+printf "Enabling FDE.\\n"
 fdesetup enable  > /dev/null 2>&1
-
 
 #Finishing Up.
 timed="$((SECONDS / 3600)) Hours $(((SECONDS / 60) % 60)) Minutes $((SECONDS % 60)) seconds"
-printf "It took %s to help get your Mac ready for Black Hat.\n" "$timed"
+printf "It Took %s To Enable MacOS Baseline Security Settings.\\n" "$timed"
